1b5a3735a923b7449ebcbbe5662b5096d69c4d10
qoth-0 <bny1324@naver.com>
2023-11-28 17:41:33 +0900

    .

---
+++ git_syntax.sh
@@ -0,0 +1,7 @@
+git add . # 전부 추가사항에 올린다
+git add test2.txt # test2.txt만 추가사항에 올린다
+git commit -m "commit message" # commit
+git branch # 현재 branch 확인
+git push origin main # 원격 main branch에 push 하겠다.
+git status # 파일 상태 확인
+git log # 커밋 이력 확인
